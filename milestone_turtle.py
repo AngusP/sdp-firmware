1340d4987f36e36995e1e3a4e6e9f73ac07abbf9
AngusP <a7cp3ar50n@gmail.com>
2016-01-26 14:13:38 +0000

    Adding to m1 c&c python script

--- milestone_turtle.py
+++ milestone_turtle.py
@@ -1,10 +1,15 @@
 #!/bin/python
 import numpy as np
+import math
 import serial
+import time
+
+move_speed = 10.0
+rot_speed = 10.0
 
 def main():
     which_port = raw_input("Serial port? (e.g. /dev/ttyACM0) ")
-    port = Serial.serial(which_port, baudrate=115200, timeot=3.0)
+    port = serial.Serial(which_port, baudrate=115200, timeout=3.0)
 
     if port is None:
         raise Exception("Couldn't open port "+which_port)
@@ -13,8 +18,8 @@ def main():
     disp_y = float(raw_input("y (forward) displacement (cm): "))
     rotation = float(raw_input("rotational difference (degrees, cw): "))
 
-    disp = np.sqrt(np.power(disp_x,2) + np.power(disp_x,2))
-    disp_rot = np.arctan(disp_y / disp_x) * 360.0
+    disp = math.sqrt(pow(disp_x,2) + pow(disp_y,2))
+    disp_rot = np.arctan(disp_y / disp_x) * (180.0 / np.pi)
 
     print "Displacement is",
     print disp
@@ -22,7 +27,19 @@ def main():
     print disp_rot,
     print "degrees"
 
+    move_time = disp / move_speed
+    rot_time = disp_rot / rot_speed
+
+    print "Exectuting move..."
+    # rotate
     
+    # forward
+    port.write("M 240 -255 0\n")
+    time.sleep(move_time)
+    port.write("M 0 0 0\n")
+
+    print "Done, exiting"
+
     
 if __name__ == '__main__':
     print "Milestone 1 turtle motion c&c\n"
