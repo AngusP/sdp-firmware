1ffac6fab86ba08f35be40f34c2c8c1db62014b9
AngusP <a7cp3ar50n@gmail.com>
2016-02-27 21:35:16 +0000

    Added a script for WASD remote control of the robot

---
+++ scripts/play.py
@@ -0,0 +1,109 @@
+#!/bin/python
+import numpy as np
+import math
+import serial
+import time
+import curses
+import sys
+
+def main():
+    try:
+        which_port = sys.argv[1]
+    except IndexError:
+        which_port = '/dev/ttyACM0'
+    
+    stdscr = curses.initscr()
+    curses.cbreak()
+    curses.noecho()
+    curses.curs_set(False)
+    stdscr.keypad(1)
+
+    port = serial.Serial(which_port, baudrate=115200, timeout=3.0)
+
+    if port is None:
+        raise Exception("Couldn't open port " + which_port)
+
+    vector = [0,0,0]
+
+    while True:
+        # WASD Control for translation, QE for rotate
+        stdscr.addstr(0,0,"Hit 'x' to quit")
+        stdscr.refresh()
+        
+        key = stdscr.getch()
+        stdscr.clear()
+        
+        if key == ord('w'):
+            # Forward
+            stdscr.addstr(1,0,"forward")
+            if vector[1] == 0:
+                vector[1] = 1
+            else:
+                vector[1] = 0
+            continue
+
+        elif key == ord('s'):
+            # Backward
+            stdscr.addstr(1,0,"backward")
+            if vector[1] == 0:
+                vector[1] = -1
+            else:
+                vector[1] = 0
+            continue
+
+        elif key == ord('a'):
+            # Left
+            stdscr.addstr(1,0,"left")
+            if vector[0] == 0:
+                vector[0] = 1
+            else:
+                vector[0] = 0
+            continue
+
+        elif key == ord('d'):
+            # Right
+            stdscr.addstr(1,0,"right")
+            if vector[0] == 0:
+                vector[0] = -1
+            else:
+                vector[0] = 0
+            continue
+
+        elif key == ord('q'):
+            # Right
+            stdscr.addstr(1,0,"rotate left")
+            if vector[2] == 0:
+                vector[2] = 1
+            else:
+                vector[2] = 0
+            continue
+
+        elif key == ord('e'):
+            # Right
+            stdscr.addstr(1,0,"rotate right")
+            if vector[2] == 0:
+                vector[2] = -1
+            else:
+                vector[2] = 0
+            continue
+
+        elif key == ord('x'):
+            curses.nocbreak()
+            stdscr.keypad(0)
+            curses.echo()
+            curses.endwin()
+            port.write("G 0 0 0\n")
+            return
+        
+        port.write("G " + 
+                   str(vector[0]) + " " + 
+                   str(vector[1]) + " " + 
+                   str(vector[2]) + "\n")
+
+        stdscr.refresh()
+        
+    
+if __name__ == '__main__':
+    main()
+    
+
